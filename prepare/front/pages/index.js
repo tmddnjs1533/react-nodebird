f5b5d9cbbcb2bb142634e37fdc11f38eb142dba8
김승원 <tmddnjs1533@gmail.com>
2022-01-14 19:32:34 +0900

    first-commit

---
+++ prepare/front/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+
+    <div>
+      Hello world!
+    </div>
+    </AppLayout>
+  );
+};
+
+export default Home;
